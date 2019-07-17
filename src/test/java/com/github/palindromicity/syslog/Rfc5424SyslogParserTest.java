f072eca6042813cccde257267d79d37438d6d8e3
Otto Fowler <ottobackwards@gmail.com>
2019-07-17 00:30:52 -0400

    fix for checkstyle

--- src/test/java/com/github/palindromicity/syslog/Rfc5424SyslogParserTest.java
+++ src/test/java/com/github/palindromicity/syslog/Rfc5424SyslogParserTest.java
@@ -36,17 +36,20 @@ public class Rfc5424SyslogParserTest extends AbstractRfc5425SyslogParserTest {
   private static final String SYSLOG_LINE_ESC_QUOTES = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
       + " d0602076-b14a-4c55-852a-981e7afeed38 DEA MSG-01"
       + " [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"]"
-      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other \\\"so called \\\" Application\" eventID=\"2022\"] Removing instance";
+      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other \\\"so called \\\" Application\" eventID=\"2022\"]"
+      + " Removing instance";
 
   private static final String SYSLOG_LINE_ESC_SLASH = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
       + " d0602076-b14a-4c55-852a-981e7afeed38 DEA MSG-01"
       + " [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"]"
-      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other \\\\so called \\\\ Application\" eventID=\"2022\"] Removing instance";
+      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other \\\\so called \\\\ Application\" eventID=\"2022\"]"
+      + " Removing instance";
 
   private static final String SYSLOG_LINE_ESC_RIGHT_BRACKET = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
       + " d0602076-b14a-4c55-852a-981e7afeed38 DEA MSG-01"
       + " [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"]"
-      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other [so called \\] Application\" eventID=\"2022\"] Removing instance";
+      + " [exampleSDID@32480 iut=\"4\" eventSource=\"Other [so called \\] Application\" eventID=\"2022\"]"
+      + " Removing instance";
 
 
   private static final String SYSLOG_LINE_NO_MSG = "<14>1 2014-06-20T09:14:07+00:00 loggregator"
